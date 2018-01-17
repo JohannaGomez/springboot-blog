a968599db00f877e830f0f27d9272a7caaa3b20c
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-17 10:22:32 -0600

    working on curriculum exercises

--- src/main/java/com/codeup/springbootblog/PostsController.java
+++ src/main/java/com/codeup/springbootblog/PostsController.java
@@ -1,5 +1,14 @@
 package com.codeup.springbootblog;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class PostsController {
 
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public int division(@PathVariable int num1, @PathVariable int num2){
+        return num1 / num2;
+    }
 }
