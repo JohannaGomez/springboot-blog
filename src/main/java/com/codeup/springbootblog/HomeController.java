043476df344cfdf2cb19935e493182b8faf4da6f
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-17 10:21:06 -0600

    including application and example properties files in gitignore

---
+++ src/main/java/com/codeup/springbootblog/HomeController.java
@@ -0,0 +1,41 @@
+package com.codeup.springbootblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String homeController () {
+        return "This is the landing page!";
+    }
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public int addition(@PathVariable int num1, @PathVariable int num2){
+        return num1 + num2;
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public int substraction(@PathVariable int num1, @PathVariable int num2){
+        return num2 - num1;
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public int multiplication(@PathVariable int num1, @PathVariable int num2){
+        return num1 * num2;
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public int division(@PathVariable int num1, @PathVariable int num2){
+        return num1 / num2;
+    }
+
+}
---
+++ src/main/java/com/codeup/springbootblog/PostsController.java
@@ -0,0 +1,5 @@
+package com.codeup.springbootblog;
+
+public class PostsController {
+
+}
