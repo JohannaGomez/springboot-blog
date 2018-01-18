fc9b2845ee742de07874fed81a15075ed0bbf04f
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-18 10:37:49 -0600

    roll-dice exercise finished

--- src/main/java/com/codeup/springbootblog/HelloWorldController.java
+++ src/main/java/com/codeup/springbootblog/HelloWorldController.java
@@ -12,12 +12,12 @@ public class HelloWorldController {
 
     @GetMapping("/say/{greeting}/to/{name}")
     @ResponseBody
-    public String greet(@PathVariable String greeting, @PathVariable String name){
+    public String greet(@PathVariable String greeting, @PathVariable String name) {
         return greeting + " " + name + "!";
     }
 
     @GetMapping("/hello")
-    public String helloWorld () {
+    public String helloWorld() {
         return "hello";
     }
 
@@ -27,33 +27,5 @@ public class HelloWorldController {
         return "hello";
     }
 
-
-//    public static int randomWithRange(int min, int max) {
-//        int range = (max - min) + 1;
-//        return (int)(Math.random() * range) + min;
-//    }
-
-    @GetMapping("/roll-dice/{num1}")
-    public String rollDice(@PathVariable int num1, Model viewModel) {
-        viewModel.addAttribute("num1", num1);
-        return "roll-dice";
-
-    }
 }
-
-
-
-//    public static void rollDiceGame(Scanner sc) {
-//        String userChoice;
-//        System.out.print("Please enter the number of sides for the dice: ");
-//        int nSides = getInteger(1, 20);
-//        do {
-//            System.out.println("You have rolled a " + randomWithRange(1, nSides) + " and a " + randomWithRange(1, nSides));
-//            do {
-//                System.out.println("Would you like to roll again? [y/n]");
-//                userChoice = sc.next().trim();
-//            } while (!userChoice.equalsIgnoreCase("y") & !userChoice.equalsIgnoreCase("n"));
-//
-//        } while (!userChoice.equals("n"));
-//    }
 
