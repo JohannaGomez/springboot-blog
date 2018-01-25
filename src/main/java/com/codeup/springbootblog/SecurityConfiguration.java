4f68dcfa552d586f6f7aced504266e9d3315f3b0
Johanna Gomez <pao_gomez1@hotmail.com>
2018-01-25 10:22:53 -0600

    logout in process

--- src/main/java/com/codeup/springbootblog/SecurityConfiguration.java
+++ src/main/java/com/codeup/springbootblog/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.codeup.springbootblog;
 
+import com.codeup.springbootblog.services.UserDetailsLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,26 +12,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//    private UserDetailsLoader usersLoader;
-//
-//    public SecurityConfiguration(UserDetailsLoader usersLoader) {
-//        this.usersLoader = usersLoader;
-//    }
-//
+    private UserDetailsLoader userDetailsLoader;
+
+    public SecurityConfiguration(UserDetailsLoader userDetailsLoader) {
+        this.userDetailsLoader = userDetailsLoader;
+    }
+
+
     @Bean
     // telling that this object should be built automaticly
     public PasswordEncoder passwordEncoder() {
         //Hashing passwords:
         return new BCryptPasswordEncoder();
     }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(usersLoader) // How to find users by their username
-//                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
-//        ;
-//    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsLoader) // How to find users by their username
+                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
+        ;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,10 +42,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .defaultSuccessUrl("/posts") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
-        .and()
+            /* Logout configuration */
+            .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout") // append a query string value
+            /* Pages that can be viewed without having to log in */
+            .and()
                 .authorizeRequests()
-                .antMatchers("/", "/posts") // anyone can see the home and posts page
+                .antMatchers("/", "/posts") // anyone can see the home and the ads pages
                 .permitAll()
-                ;
+            /* Pages that require athentication */
+            .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/posts/create", // only authenticated users can create ads
+                        "/posts/{id}/edit" // only authenticated users can edit ads
+                )
+                .authenticated()
+        ;
     }
 }
